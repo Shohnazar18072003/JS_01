3566f7a34177cc255df53d87f7198aa3afb608f9
Shohnazar2003 <129948756+Shohnazar2003@users.noreply.github.com>
2023-08-14 10:41:56 +0500

    Task done

---
+++ main.js
@@ -0,0 +1,521 @@
+// Problem_1
+
+// A butun son berilgan. Jumlani rostlikka tekshiring: A son musbat .
+
+
+// var A = 10 ;
+
+// if (A>0) {
+//   console.log('A musbat son');
+// }
+// else if (A==0) {
+//   console.log('A=0');
+// }
+// else {
+//   console.log('A manfiy son');
+// }
+
+
+// Problem_2
+
+// A butun son berilgan. Jumlani rostlikka tekshiring: A toq son
+
+
+// var A = 13 ;
+
+// if ( A % 2 == 0 ) {
+//   console.log('A juft son');
+// }
+// else {
+//   console.log('A toq son');
+// }
+
+
+// Problem_3
+
+// A butun son berilgan. Jumlani rostlikka tekshiring: A juft son
+
+
+// var A = 18 ;
+
+// if ( A % 2 == 0 ) {
+//   console.log('A juft son');
+// }
+// else {
+//   console.log('A toq son');
+// }
+
+
+// Problem_4
+
+// Ikkita butun son berilgan. Jumlani rostlikka tekshiring. M-s a > 0 && b > 0
+
+// var a = 10 ;
+// var b = -10 ;
+
+// if ( a > 0 && b > 0 ) {
+//   console.log('true');
+// }
+// else {
+//   console.log('false');
+// }
+
+
+// Problem_5
+
+// 3 ta son berilgan a,b,c. a b dan katta b c dan katta rostlikka tekshiring ; 
+
+// var a = 10 ;
+// var b = 9 ;
+// var c = 7 ;
+
+// if ( a > b && b > c) {
+//   console.log('true');
+// }
+// else {
+//   console.log('false');
+// }
+
+
+// Problem_6 
+
+// a, b, c, son berilgan. b son a va c sonlarni orasida yotadi.
+
+// var a = 12 ;
+// var b = 15 ;
+// var c = 20 ;
+
+// if ( a < b && b < c ) {
+//   console.log('true');
+// }
+// else {
+//   console.log('false');
+// }
+
+
+// Problem_7
+
+// a, b sonlar berilgan sonlarni toq songa tekshiring.
+
+// var a = 13 ;
+// var b = 17 ;
+
+// if (a % 2 != 0 && b % 2 != 0 ) {
+//   console.log('a va b toq son');
+// }
+// else {
+//   console.log('a va b toq sonlar emas');
+// }
+
+
+// Problem_8 
+
+// a,b,c,d sonlar berilgan barchasi musbat
+
+// var a = 1 ;
+// var b = 20 ;
+// var c = 15 ;
+// var d = 12 ;
+
+// if ( a > 0 && b > 0 && c > 0 && d > 0) {
+//   console.log('Sonlarni hammasi musbat');
+// }
+// else {
+//   console.log('Sonlarni hammasi musbat emas');
+// }
+
+
+// Problem_9
+
+// a,b,c sonlar berilgan kamida ikkitasi musbat berilgan
+
+// var a = 10 ;
+// var b = 20 ; 
+// var c = -100;
+
+// if (a > 0 && b > 0 && c < 0 || a < 0 && b > 0 && c > 0 || a > 0 && b < 0 && c > 0 ) {
+//   console.log('Berilgan sonlarni kamida 2 tasi musbat');
+// }
+// else {
+//   console.log('Berilgan sonlarni kamida 2 tasi musbat emas');
+// }
+
+
+// Problem_10
+
+// hafta kunlari berilgan raqamga qarab hafta kunlarini nomini chiqaring.
+
+// var a = 7 ;
+
+// if ( a == 1 ) {
+//   console.log('Dushanba');
+// }
+// else if ( a == 2 ) {
+//   console.log('Seshanba');
+// }
+// else if ( a == 3 ) {
+//   console.log('Chorshanba');
+// }
+// else if ( a == 4 ) {
+//   console.log('Payshanba');
+// }
+// else if ( a == 5 ) {
+//   console.log('Juma');
+// }
+// else if ( a == 6 ) {
+//   console.log('Shanba');
+// }
+// else if ( a == 7 ) {
+//   console.log('Yakshanba');
+// }
+
+
+// Problem_11
+
+// Telefon nomer kodi orqali qaysi kompaniya ekanligini tekshiring.
+
+// var a = 33 ;
+
+// if ( a == 99 ) {
+//   console.log('Uzmobile');
+// }
+// else if ( a == 94 || a == 93 ) {
+//   console.log('Ucell');
+// }
+// else if ( a == 90 || a == 91 ) {
+//   console.log('Beeline');
+// }
+// else if ( a == 97 || a == 55 ) {
+//   console.log('MobiUz');
+// }
+// else if ( a == 33 ) {
+//   console.log('Humans');
+// }
+
+
+// Problem_12
+
+// Butun son berilgan agar son musbat bo'lsa unda sonni bittaga oshiring aks xolda 1taga kamaytiring.
+
+// var a = 10 ;
+
+// if ( a > 0 ) {
+//   console.log(++a);
+// }
+// else {
+//   console.log(--a);
+// }
+
+
+// Problem_13 
+
+// Butun son berilgan agar son musbat bo'lsa sonni 3 ga oshiring manfiy bo'lsa unda 2ga kamaytiring.
+
+// var a = 700 ;
+
+// if ( a > 0 ) {
+//   console.log(a+3);
+// }
+// else {
+//   console.log(a-2);
+// }
+
+
+// Problem_14
+
+// Ikkita butun son berilgan kattasini aniqlovchi dastur tuzing.
+
+// var a = 19 ;
+// var b = 26 ;
+
+// if ( a > b ) {
+//   console.log('a son katta');
+// }
+// else {
+//   console.log('b son katta');
+// }
+
+
+// Problem_15 
+
+// 3 ta butun son berilgan eng kattasini aniqlovchi dastur tuzing.
+
+//  var x=5;
+//  var y=10;
+//  var z=15;
+
+// 	if(x>y && x>z) {
+//     console.log("X = "+x+" is the greatest");
+//   }
+// 	else if(z>x && z>y) {
+//     console.log("Z = "+z+" is the greatest");
+//   }
+// 	else {
+//     console.log("Y = "+y+" is the greatest");
+//   } 
+
+
+// Problem_16 
+
+// 3 ta son berilgan eng kichikini aniqlovchi dastur tuzing.
+
+//  var x=5;
+//  var y=10;
+//  var z=15;
+
+// 	if(x<y && x<z) {
+//     console.log("X = "+x+" is the smallest number");
+//   }
+// 	else if(z<x && z<y) {
+//     console.log("Z = "+z+" is the smallest number");
+//   }
+// 	else {
+//     console.log("Y = "+y+" is the smallest number");
+//   } 
+
+
+// Problem_17
+
+// Jinsga qarab log chiqivchi dastur tuzing!
+
+// var x = "erkak" ;
+// var x = "ayol" ;
+
+// if ( x == "erkak") {
+//   console.log(x);
+// }
+// else if ( x == "ayol" ) {
+//   console.log(x);
+// }
+
+
+// Problem_18 
+
+// Bahoga qarab stependiyani aniqlovchi dastur tuzing.
+
+// var a = 5 ;
+
+// if ( a == 5 ) {
+//   console.log('Siz 5 lik stipendiya olasiz');
+// }
+// else if ( a == 4 ) {
+//   console.log('Siz 4 lik stipendiya olasiz');
+// }
+// else if ( a == 3 ) {
+//   console.log('Siz 3 lik stipendiya olasiz');
+// }
+
+
+// Problem_19 
+
+// Butun son berilgan shu songa qarab toq musbat, toq manfiy, juft musbat, juft manfiy deb chiqaruvchi dastur tuzing.
+
+// var a = -12 ;
+
+// if ( a > 0 && a % 2 == 0) {
+//   console.log('juft musbat son');
+// }
+// else if ( a > 0 && a % 2 != 0 ) {
+//   console.log('toq musbat son');
+// }
+// else if ( a < 0 && a % 2 != 0 ) {
+//   console.log('toq manfiy son');
+// }
+// else if ( a < 0 && a % 2 == 0 ) {
+//   console.log('juft manfiy son');
+// }
+
+
+// Problem_20 
+
+// Yilga qarab yoshini aniqlovchi dastur tuzing.
+
+// var a = 2005 ;
+
+// if ( a == 2000 ) {
+//   console.log('23yosh');
+// }
+// else if ( a == 2001) {
+//   console.log('22yosh');
+// }
+// else if ( a == 2002) {
+//   console.log('21yosh');
+// }
+// else if ( a == 2003) {
+//   console.log('20yosh');
+// }
+// else if ( a == 2004) {
+//   console.log('19yosh');
+// }
+// else if ( a == 2005) {
+//   console.log('18yosh');
+// }
+// else if ( a == 2006) {
+//   console.log('17yosh');
+// }
+// else if ( a == 2007) {
+//   console.log('16yosh');
+// }
+// else if ( a == 2008) {
+//   console.log('15yosh');
+// }
+// else if ( a == 2009) {
+//   console.log('14yosh');
+// }
+// else if ( a == 2010) {
+//   console.log('13yosh');
+// }
+// else if ( a == 2011) {
+//   console.log('12yosh');
+// }
+// else if ( a == 2012) {
+//   console.log('11yosh');
+// }
+// else if ( a == 2013) {
+//   console.log('10yosh');
+// }
+// else if ( a == 2014) {
+//   console.log('9yosh');
+// }
+// else if ( a == 2015) {
+//   console.log('8yosh');
+// }
+
+
+// Problem_21
+
+// 3 ta butun son berilgan jumlani rostlikkar tekshiring. A <= B <= C
+
+// var A = 22 ;
+// var B = 80 ;
+// var C = 111 ;
+
+// if ( A <= B && B <= C ) {
+//   console.log('true');
+// }
+// else {
+//   console.log('false');
+// }
+
+
+// Problem_22
+
+// ikki butun son berilgan ikki sondan faqat 1tasi toq son hisoblanadi.
+
+// var a = 13 ;
+// var b = 16 ;
+
+// if ( a % 2 != 0 &&  b % 2 == 0 || b % 2 != 0 && a % 2 == 0) {
+//   console.log('true');
+// }
+// else {
+//   console.log('false');
+// }
+
+
+// Problem_23
+
+// a,b,c sonlar berilgan har biri musbat son
+
+// var a = 14 ;
+// var b = 45 ;
+// var c = -89 ;
+
+// if ( a > 0 && b > 0 && c > 0 ) {
+//   console.log('Berilgan sonlarning hammasi musbat son');
+// }
+// else {
+//   console.log('Berilgan sonlarning hammasi musbat son emas');
+// }
+
+
+// Problem_24 
+
+// a,b,c sonlardan biri musbat son 
+
+// var a = 20 ;
+// var b = -30 ;
+// var c = -18 ;
+
+// if ( a > 0 && b < 0 && c < 0 || a < 0 && b > 0 && c < 0 || a < 0 && b < 0 && c > 0) {
+//   console.log('Berilgan sonlardan biri musbat son');
+// }
+// else {
+//   console.log('Berilgan sonlardan hech biri musbat son emas');
+// }
+
+
+// Problem_25
+
+// 3 xonali son berilgan har bir raqamlari har xil
+
+// function checkDifferentDigits(number) {
+//   var numString = number.toString();
+
+//   if (numString[0] !== numString[1] && numString[0] !== numString[2] && numString[1] !== numString[2]) {
+//     console.log("The number has different digits.");
+//   } else {
+//     console.log("The number does not have different digits.");
+//   }
+// }
+// checkDifferentDigits(999);
+
+
+// Problem_26 
+
+// 3 xonali son berilgan raqamlari ketma ket o'sib boruvchi joylashgan.
+
+// function checkSequentialDigits(number) {
+//   var numString = number.toString();
+
+//   if (numString[0] < numString[1] && numString[1] < numString[2]) {
+//     console.log("The number has sequentially increasing digits.");
+//   } else {
+//     console.log("The number does not have sequentially increasing digits.");
+//   }
+// }
+// checkSequentialDigits(123);
+// checkSequentialDigits(321);
+
+
+// Problem_27 
+
+// Kompyuter nomiga qarab kompyuter summasini chiqaruvchi dastur yarating.
+
+// function generateComputerSum(computerName) {
+//   var sum = 0;
+//   var lowercaseName = computerName.toLowerCase();
+
+//   for (var i = 0; i < lowercaseName.length; i++) {
+//     var charCode = lowercaseName.charCodeAt(i);
+
+//     if (charCode >= 97 && charCode <= 123) {
+//       sum += charCode - 96; 
+//     }
+//   }
+
+//   return sum;
+// }
+// var computerName = "HP VICTUS";
+// var computerSum = generateComputerSum(computerName);
+// console.log("Computer Sum:", computerSum);
+
+
+// Problem_28 
+
+// Sonlar berilgan shu sonlar ichidan faqat murakkab sonlarni chiqaruvchi dastur tuzing (for)! 
+
+function generateComplexNumbers(numbers) {
+  var complexNumbers = [];
+
+  for (var i = 0; i < numbers.length; i++) {
+    var complexNumber = numbers[i] + "i";
+    complexNumbers.push(complexNumber);
+  }
+
+  return complexNumbers;
+}
+
+var givenNumbers = [2, 5, 8];
+var complexNumbers = generateComplexNumbers(givenNumbers);
+console.log(complexNumbers);
